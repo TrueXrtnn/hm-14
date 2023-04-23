705950579630989e46642e243cbbd041e892d6be
true.xrstn <mr.chareev@mail.ru>
2023-04-23 13:29:46 +0800

    =

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,5 @@
 public class Main {
-    public static void authorization(String login, String password, String confirmPassword){
-        if (login.length()>20&&login.contains("[^\da-zA-Z_]")){
 
-        }
-        if (password.equals(confirmPassword)){
-
-        }
-        if (){
-
-        }
-    }
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
